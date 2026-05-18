0654da0bb950267e8ec026d2f98574380945dfb4
MickVillegas <mickprofesional@gmail.com>
2026-05-18 13:01:01 +0200

    he creado componentes para actualizar y crear personas

--- src/peticiones.jsx
+++ src/peticiones.jsx
@@ -6,6 +6,7 @@ export default function App(){
         <p>Hola pendejos</p>
         <TodasPersonas />
         <VerUnaPersona />
+
     </div>
     )
 }
@@ -21,6 +22,20 @@ async function buscarPersonas() {
 
 export function TodasPersonas() {
     const [personas, setPersonas] = useState([]);
+    const [mostrar, setMostrar] = useState(false)
+    let [numero, setNumero] = useState("")
+
+    function mostrarono(x){
+        setNumero(x)
+        console.log("Numero guardado " + x)
+
+        if(mostrar == true){
+            setMostrar(false)
+        }
+        else{
+            setMostrar(true)
+        }
+    }
 
     useEffect(() => {
         async function buscarPersonas() {
@@ -35,6 +50,17 @@ export function TodasPersonas() {
         buscarPersonas();
     }, []); // El array vacío [] hace que solo se ejecute UNA vez al cargar.
 
+    const [formularioCrear, setFormularioCrear] = useState(false)
+
+function mostrarFormularioCrear(){
+    if(formularioCrear == false){
+        setFormularioCrear(true)
+    }
+    else{
+        setFormularioCrear(false)
+    }
+}
+
     return (
         <div>
             <p>Personas buscadas:</p>
@@ -42,8 +68,13 @@ export function TodasPersonas() {
                     <div id={p.id}>
                         <h1>{p.nombre}</h1>
                         <p>{p.edad}</p>
+                        <button onClick={() => mostrarono(p.id)}>Actualizar</button>
                     </div>
                 ))}
+                {mostrar && <MostrarFormulario  meterId ={numero}/>}
+                <br />
+                <button onClick={mostrarFormularioCrear}>Crear persona</button>
+                {formularioCrear && <CrearPersona />}
         </div>
     );
 }
@@ -69,4 +100,67 @@ export function VerUnaPersona(){
             <p>{unaPer.edad}</p>
         </div>
     )
+}
+
+async function obtenerNumero(id){
+        console.log("esto funcionará? " + id)
+
+    let nombre = document.getElementById("nombre").value
+    let edad  = document.getElementById("edad").value
+
+    let persona = {
+        nombre: nombre,
+        edad: edad
+    }
+
+    try {
+        let rep = await fetch("http://localhost:3000/actualizarPersona/ " + id, {
+            method: "PUT",
+            body: JSON.stringify(persona),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+
+}
+
+function MostrarFormulario({meterId}){
+    //obtenerNumero(meterId)
+    return (<div><input type='text' id = "nombre" placeholder='Nombre'></input><input type='text' id = "edad" placeholder='Edad'></input> <button onClick={()=>obtenerNumero(meterId)}>Enviar</button></div>)
+}
+
+
+async function subirPersona(){
+
+    let nombre = document.getElementById("crenombre").value
+    let edad  = document.getElementById("creedad").value
+
+    let persona = {
+        nombre: nombre,
+        edad: edad
+    }
+
+    try {
+        let rep = await fetch("http://localhost:3000/crearPersona", {
+            method: "POST",
+            body: JSON.stringify(persona),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+    catch(error){
+        console.log(error)
+    }
+
+}
+
+function CrearPersona(){
+    return (<div><input type='text' id = "crenombre" placeholder='Nombre'></input><input type='text' id = "creedad" placeholder='Edad'></input> <button onClick={() => subirPersona()}>Enviar</button></div>)
 }
